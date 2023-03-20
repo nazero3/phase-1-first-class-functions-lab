0e031e9acd44da656d8e3a8178fab497531614ae
nazero3 <nazerrabah1994@gmail.com>
2023-03-20 16:40:21 +0300

    needs to be completed 4th question and later

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+        return [arr[0],arr[1]];
+}
+
+const returnLastTwoDrivers = function(arr){
+    return [arr[arr.length-2],arr[arr.length-1]]
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
